300e76ba6a47106b4164407897d1481ab8b064e6
Maria Dolgareva <dolgareva.ma@gmail.com>
2026-05-16 12:25:51 -0400

    nav fixes

--- menu.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-  document.getElementById('nav-mobile').classList.toggle('open');
-  document.getElementById('mobile-overlay').classList.toggle('open');
-  document.body.classList.toggle('menu-open');
-}
